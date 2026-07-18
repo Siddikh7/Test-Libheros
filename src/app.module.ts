5b1f7f5bfa8f6084bcd126e04711d8daa923a4ae
diab0981 <boubacar.siddikh.diallo@usherbrooke.ca>
2026-07-19 00:17:06 +0200

    Premier commit

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const dbPort = Number(configService.get<string>('DB_PORT', '5432'));
+
+        return {
+          type: 'postgres' as const,
+          host: configService.get<string>('DB_HOST', 'localhost'),
+          port: dbPort,
+          username: configService.get<string>('DB_USERNAME', 'postgres'),
+          password: configService.get<string>('DB_PASSWORD', 'postgres'),
+          database: configService.get<string>('DB_NAME', 'libheros'),
+          autoLoadEntities: true,
+          synchronize: false,
+        };
+      },
+    }),
+  ],
+})
+export class AppModule {}
